726422f6aa2e201dee63db3c402e7b4db88a751c
Lucas Almeida <joxsal01@easv365.dk>
2026-09-03 10:44:14 +0200

    Subdoamin

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 import { Selector } from 'testcafe';
 fixture('Pressing enter key should add a new todo item')
-    .page('http://lucasalmeida.dk/test/todo/#/');
+    .page('http://test.lucasalmeida.dk/todo/#/');
     
     test('Input value can be added with enter key', async t => {
         // Arange
