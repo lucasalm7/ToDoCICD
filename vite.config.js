f5d7c202af059ecfa670a37fe80562968da53271
Lucas Almeida <joxsal01@easv365.dk>
2026-09-03 10:32:50 +0200

    add tests

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo/',
+  base: "/todo/",
   plugins: [
     vue(),
     viteStaticCopy({
